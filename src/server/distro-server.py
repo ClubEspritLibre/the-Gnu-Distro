407d8ba8af9d3f6ec4f31b1c92431a88b069352e
Alexandre BM <s@rednaks.tn>
2013-03-21 11:11:08 +0100

    [SERVER] main script, serving ws

--- src/server/distro-server.py
+++ src/server/distro-server.py
@@ -9,4 +9,12 @@
 # 2 Espaces pour un niveau d'indentation.
 
 
+from simpleWSpy.server import WebSocketServer
+from command.handler import Handler
+
+
+if __name__ == '__main__' :
+  handler = Handler()
+  ws = WebSocketServer.WebSocket(('localhost', 9999))
+  ws.serve_forever(handler)
 
