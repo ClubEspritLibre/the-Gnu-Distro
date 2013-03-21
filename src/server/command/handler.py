05cef1d09454c61b820c1da09828856243c31cb1
Alexandre BM <s@rednaks.tn>
2013-03-21 11:44:18 +0100

    [SERVER] Implementing extractArgs function and adding args attribute to the handler class

--- src/server/command/handler.py
+++ src/server/command/handler.py
@@ -2,10 +2,15 @@
 
 class Handler():
   def __init__(self):
-    self.stream = ''
+    self.stream = ''     #contiendra le message reçu de la part du serveur
+    self.args = []       # la liste d'arguments 
 
   def onReceive(self, aStream):
     self.stream = aStream
+    self.extractArgs()
   # TODO
     return None, 0
+
+  def extractArgs(self):
+    self.args = self.stream.split(':')
 
