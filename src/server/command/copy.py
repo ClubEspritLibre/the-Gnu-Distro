6282595209ebaa6e1dd9a74c2d014af33debec57
asmasma21 <assma.saidi@gmail.com>
2013-03-23 13:27:34 +0100

    [Server] Verify if the usb device is connected

--- src/server/command/copy.py
+++ src/server/command/copy.py
@@ -2,5 +2,10 @@ import os
 import getpass
 
 def copyDistribution(dist):
-  result= os.popen("udisks --show-info /dev/disk/by-id/usb*part1|grep -i 'mount paths'").read()
-  target=result[result.find("/"):]  
+  usb = [f for f in os.listdir("/dev/disk/by-id/") if f.find("usb")!= -1 ]
+  if(len(usb)!=0):
+    result= os.popen("udisks --show-info /dev/disk/by-id/usb*part1|grep -i 'mount paths'").read()
+    target=result[result.find("/"):]
+    return True
+  else:
+    return False
