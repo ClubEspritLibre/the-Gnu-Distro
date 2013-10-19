f56f06cabbea281f788aa1dd4688cf8e636b7142
modamod <modhaffer.rahmani@gmail.com>
2013-10-19 21:43:09 +0100

    this will get the job done for the os.popen => subprocess module switch

--- src/server/command/copy.py
+++ src/server/command/copy.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-import os,shutil
+import os, shutil, subprocess
 from utils import usbIsPresent
 
 def copyDistribution(dist):
-  # usb = [f for f in os.listdir("/dev/disk/by-id/") if f.find("usb")!= -1 ]
+  usb = [f for f in os.listdir("/dev/disk/by-id/") if f.find("usb") != -1]
+  print usb
+  print usbIsPresent()
   if usbIsPresent() == True:
     homedir = os.environ['HOME']
     base = homedir+"/.the-gnu-distro/distribution/"+dist
-    result = os.popen("udisks --show-info /dev/disk/by-id/usb*part1|grep -i 'mount paths'").read()
-    target = result[result.find("/"):len(result)-1]
+    result = subprocess.Popen("udisks --show-info /dev/disk/by-id/usb*part1 | grep -i 'mount paths'",
+                           stdout=subprocess.PIPE, shell=True).stdout.read()
+
+    target = result[result.find("/"):-1]
     try:
       shutil.copy(base,target)
     except IOError, e:
